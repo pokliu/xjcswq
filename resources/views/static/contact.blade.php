3b9c538ca1ba355a64a73a103df8b24fb92312f9
poklau <747364155@qq.com>
2018-04-08 15:21:41 +0800

    add qq link

--- resources/views/static/contact.blade.php
+++ resources/views/static/contact.blade.php
@@ -23,7 +23,11 @@
 <p>E－mail：xurong@vtlaw.cn</p>
 
 <p>虚假陈述维权网同时提供在线咨询功能，若您有更多疑问，通过以下渠道可与我们在线进行联系，我们致力于及时为您的虚假陈述维权提供解决方案。</p>
-<p>在线咨询:--</p>
+<p>
+    <span>在线咨询:</span>
+    <a target="_blank" href="http://wpa.qq.com/msgrd?v=3&uin=504900228&site=qq&menu=yes"><img border="0" src="http://wpa.qq.com/pa?p=2:504900228:41" alt="点击这里给我发消息" title="点击这里给我发消息"/></a>
+    <a target="_blank" href="http://wpa.qq.com/msgrd?v=3&uin=504900228&site=qq&menu=yes"><img border="0" src="http://wpa.qq.com/pa?p=2:504900228:41" alt="点击这里给我发消息" title="点击这里给我发消息"/></a> 
+</p>
 
 <p>来信、来函、材料请寄：广东省深圳市福田区金田路4028号荣超经贸中心45层 万商天勤（深圳）律师事务所</p>
 
