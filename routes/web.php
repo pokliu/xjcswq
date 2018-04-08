5526d4dd0d2a79ff5f037e1cc47177152f05314a
poklau <747364155@qq.com>
2018-04-08 17:40:39 +0800

    quick fix

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,19 @@ Route::view('/static/agreement', 'static.agreement');
 
 Route::get('/admin', 'AdminController@index');
 
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 if(env('APP_DEBUG', false) == true){
-    Auth::routes();
+    // Registration Routes...
+    Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+    Route::post('register', 'Auth\RegisterController@register');
 }
 
 Route::resource('post', 'PostController');
